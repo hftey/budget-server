bd7f94d87bc466be3634d4e00aceb631d357b1da
raymond.tey <raymond.tey@gmail.com>
2020-05-11 10:16:59 +1000

    remove gitignore files

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
     // Website you wish to allow to connect
     //res.setHeader('Access-Control-Allow-Origin', 'http://budget-ngx.venzon-solution.com');
 
-    var allowedOrigins = ['http://budget-ngx.venzon-solution.com', 'http://budget.macpro:4200'];
+    var allowedOrigins = ['http://budget-ngx.venzon-solution.com', 'http://localhost:8100'];
     var origin = req.headers.origin;
     if(allowedOrigins.indexOf(origin) > -1){
         res.setHeader('Access-Control-Allow-Origin', origin);
